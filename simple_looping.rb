45173b971f64ea53e5096240231627b653df97b3
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-11 19:32:24 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -42,6 +42,9 @@ def until_iterator(number_of_times)
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
+  until number_of_times == 0
+  end
+
 end
 
 def for_iterator(number_of_times)
